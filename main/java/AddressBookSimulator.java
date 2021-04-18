675f27d497c5ab8a3246399ad1ddc8fb501f3287
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-18 15:59:28 +0530

    STARTED: Address book simulator program with welcome message

---
+++ main/java/AddressBookSimulator.java
@@ -0,0 +1,8 @@
+/**
+ * main class
+ */
+public class AddressBookSimulator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to address book simulator!");
+    }
+}
