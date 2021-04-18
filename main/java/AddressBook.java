391a5fe194a8963f37a119f4ea5d024b9c1cca56
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2021-04-18 16:44:20 +0530

    ADD: Method to add new contacts from console

--- main/java/AddressBook.java
+++ main/java/AddressBook.java
@@ -1,15 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressBook {
+    List<Contacts> contactList = new ArrayList<>();
+    ScannerForAddressBook scannerForAddressBook = new ScannerForAddressBook();
+    
+    /**
+     * method to add new contact from the console
+     */
     public void addContact() {
-        Contacts contact = new Contacts();
+        Contacts contacts = new Contacts();
         
-        contact.setFirstName("Vivek");
-        contact.setLastName("Kolasani");
-        contact.setAddress("Near home");
-        contact.setCity("Hyderabad");
-        contact.setState("TS");
-        contact.setEmail("vivek.kolasani@gmail.com");
-        contact.setZip(102563);
-        contact.setPhoneNumber("785964231");
-        System.out.println(contact);
+        System.out.println("Enter first name");
+        contacts.setFirstName(scannerForAddressBook.scannerProvider().nextLine());
+        
+        System.out.println("Enter last name");
+        contacts.setLastName(scannerForAddressBook.scannerProvider().nextLine());
+        
+        System.out.println("Enter address");
+        contacts.setAddress(scannerForAddressBook.scannerProvider().nextLine());
+        
+        System.out.println("Enter city");
+        contacts.setCity(scannerForAddressBook.scannerProvider().nextLine());
+        
+        System.out.println("Enter state");
+        contacts.setState(scannerForAddressBook.scannerProvider().nextLine());
+        
+        System.out.println("Enter email");
+        contacts.setEmail(scannerForAddressBook.scannerProvider().nextLine());
+        
+        System.out.println("Enter zip");
+        contacts.setZip(scannerForAddressBook.scannerProvider().nextInt());
+        
+        System.out.println("Enter phone number");
+        contacts.setPhoneNumber(scannerForAddressBook.scannerProvider().nextLine());
+        
+        contactList.add(contacts);
     }
 }
